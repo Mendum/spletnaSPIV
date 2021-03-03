52cfcd107676563ac93e762502b0f756f6a3d89e
amadejj5000 <45545201+amadejj5000@users.noreply.github.com>
2021-03-03 18:36:40 +0100

    created default MVC project

---
+++ spletnaSPIV/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace spletnaSPIV.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
