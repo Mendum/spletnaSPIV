41c83e0e23a9f212d3f82b9029d7111750736af0
amadejj5000 <45545201+amadejj5000@users.noreply.github.com>
2021-03-04 18:05:47 +0100

    added user class for login/register/profile

---
+++ spletnaSPIV/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace spletnaSPIV.Models
+{
+    public class User
+    {
+        public string Id { get; set; }
+        public string Ime { get; set; }
+        public string Priimek { get; set; }
+        public string Email { get; set; }
+        public string Geslo { get; set; }
+
+    }
+}
